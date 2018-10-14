97146f9bf56057ebe8485d34919cf1d494b167c2
jongwony <lastone9182@gmail.com>
2018-10-14 22:59:05 +0900

    crawling schema design

--- contentScript.js
+++ contentScript.js
@@ -1,5 +1,15 @@
 window.addEventListener("load", function(event) {
   console.log('contentScript.js load success');
   console.log('event:', event);
-  localStorage.setItem('stackoverflow', window.location.href);
+  let {host, href} = window.location;
+  let body = document.body.innerText;
+  let d = {};
+  let v = {timestamp: Date.now()};
+  v[href] = body;
+  d[host] = v;
+  chrome.storage.sync.set(
+    d,
+    function() {
+      console.log(host, 'add successfully')
+    })
 });
